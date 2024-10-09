900e381683b943e484bbd251e662386fcdf43be7
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2024-10-10 10:38:13 +1100

    Copy parts to clipboard separatelly

--- src/output/clipboard.rs
+++ src/output/clipboard.rs
@@ -1,10 +1,41 @@
 use arboard::Clipboard;
 use std::error::Error;
+use std::io::{self, Write};
 
 pub fn output_to_clipboard(content: Vec<String>) -> Result<(), Box<dyn Error>> {
     let mut clipboard = Clipboard::new()?;
-    let msg = content.join("\n").trim().to_string();
-    clipboard.set_text(msg)?;
-    println!("Output copied to clipboard.");
+
+    if content.len() == 1 {
+        clipboard.set_text(content[0].trim().to_string())?;
+        println!("Output copied to clipboard.");
+    } else {
+        for (index, part) in content.iter().enumerate() {
+            clipboard.set_text(part.trim().to_string())?;
+
+            if index < content.len() - 1 {
+                println!(
+                    "Part {} of {} copied to clipboard.",
+                    index + 1,
+                    content.len()
+                );
+                println!("Press Enter to copy the next part");
+                wait_for_enter()?;
+            } else {
+                println!(
+                    "Part {} of {} copied to clipboard.",
+                    index + 1,
+                    content.len()
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn wait_for_enter() -> io::Result<()> {
+    io::stdout().flush()?;
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
     Ok(())
 }
